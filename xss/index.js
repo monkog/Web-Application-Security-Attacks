5ed7a15eb7c4a258936ed856ca031bdcd448e90b
monkog <monkog@wp.pl>
2020-01-26 17:13:25 +0100

    XSS vulnerability fix

--- xss/index.js
+++ xss/index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const sanitize = require('sanitize-html');
 const app = express();
 
 app.use(bodyParser());
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'"]
+    }
+}));
 app.use(express.static('.'));
 
 const comments = [];
 
 app.post('/comments', (request, response) => {
     const comment = request.body.comment;
+    const sanitizedComment = sanitize(comment);
+    console.log('Original: ', comment, 'sanitized: ', sanitizedComment);
     comments.push(comment);
     response.end('Saved');
 });
